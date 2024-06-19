efd73f3ba2069589dc235ad698a0fa53109d8ff7
Iryna <iryna.zaporozhets@fu-berlin.de>
2024-06-19 17:58:18 +0200

    Update cuda toolkit to 11.3

--- setup/setup_environment.sh
+++ setup/setup_environment.sh
@@ -2,4 +2,4 @@ conda env create --file environment.yml
 
 source ~/.bashrc
 conda activate picg
-pip install   torch==1.12.0+cu102 -f https://download.pytorch.org/whl/torch_stable.html
+pip install   torch==1.12.0+cu113 -f https://download.pytorch.org/whl/torch_stable.html
