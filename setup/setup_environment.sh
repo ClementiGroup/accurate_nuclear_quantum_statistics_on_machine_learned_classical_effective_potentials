d344393212bdc597457a03d0a5d3c80fcaecda20
Iryna Zaporozhets <iryna.zaporozhets@fu-berlin.de>
2024-07-03 21:05:38 +0200

    Clean up setup files

--- setup/setup_environment.sh
+++ setup/setup_environment.sh
@@ -2,4 +2,4 @@ conda env create --file environment.yml
 
 source ~/.bashrc
 conda activate picg
-# conda install pytorch==1.12.0 torchvision==0.13.0 torchaudio==0.12.0 cudatoolkit=11.3 -c pytorch -y
+conda install pytorch==1.12.0 torchvision==0.13.0 torchaudio==0.12.0 cudatoolkit=11.3 -c pytorch -y
